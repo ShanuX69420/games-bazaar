1fe8420cd9828887142cb7a286166f7a92444aa0
ShanuX69420 <you@example.com>
2025-07-11 20:53:09 +0500

    Add trusted origin for CSRF

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,10 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+    CSRF_TRUSTED_ORIGINS = [
+    'https://games-bazaar.onrender.com', # Replace with your actual URL
+]
+
 
 # Application definition
 
