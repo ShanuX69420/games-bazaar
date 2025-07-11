6b7bfd845b0f79c6369567180f5d4dbe248de626
ShanuX69420 <you@example.com>
2025-07-11 20:45:40 +0500

    Fix Cloudinary package name

--- core/settings.py
+++ core/settings.py
@@ -152,3 +152,14 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+
+# --- CLOUDINARY CONFIGURATION ---
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+# Set the default file storage to Cloudinary
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
